2c9b755791e4fdab1823015f2565ef2defa12e25
Daniel Power <danpowerdev@proton.me>
2022-11-25 10:40:42 -0500

    content skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Let's build something beautiful</h1>
       </header>
+      <section className='welcome'>
+        <h2>Welcome</h2>
+        <p>
+          I'm a software professional with over 4 years of experience in building sleek products, designing polished interfaces and writing clean code.
+        </p>
+        <p>
+          Currently seeking new clients for product, design or frontend development contracts.
+        </p>
+      </section>
+      <section>
+        <h2>What I do</h2>
+        <div>
+          <article>
+            <h3>Product management</h3>
+            <p></p>
+          </article>
+          <article>
+            <h3>UX Design</h3>
+          </article>
+          <article>
+            <h3>Frontend development</h3>
+          </article>
+        </div>
+      </section>
+      <section>
+        <h2>Portfolio</h2>
+        <div>
+          <h3>Major projects</h3>
+        </div>
+        <div>
+          <h3>Minor projects</h3>
+          <p>Small contracts, or just for fun</p>
+        </div>
+      </section>
+      <section>
+        <h2>Work with me</h2>
+      </section>
     </div>
   );
 }

7ceaec4b648c7979c0303642363388e2c7d79723
Daniel Power <danpowerdev@proton.me>
2023-01-09 15:08:04 -0500

    Basics done for tools

--- src/components/skill-card.js
+++ src/components/skill-card.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCode, faPenRuler, faListCheck } from '@fortawesome/free-solid-svg-icons'
 
@@ -17,14 +17,26 @@ export const SkillCard = ({skill}) => {
       {findIcon(title)}
       <h3>{title}</h3>
       <p>{text}</p>
-      {tools.map((tool, i) => <ToolImage key={i} tool={tool} />)}
+      <div className="tool-container">
+        {tools.map((tool, i) => <ToolImage key={i} tool={tool} />)}
+      </div>
     </article>
   )
 }
 
 const ToolImage = ({tool}) => {
-  const {src, alt} = tool
+  
+  const [isHovered, setIsHovered] = useState(false)
+  const {title, src, alt} = tool
+
   return (
-    <img className='tool-img' src={src} alt={alt} />
+    <div 
+      className='tool'
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
+    >
+      <img className='tool-img' src={src} alt={alt} />
+      {isHovered && <p className="tool-title">{title}</p>}
+    </div>    
   )
 }
--- src/content/skills.js
+++ src/content/skills.js
@@ -4,23 +4,28 @@ export const skillArray = [
     text: 'Solving techincal problems and bringing designs to life with pixel-perfect, rigorously tested accuracy...',
     tools: [
       {
+        title: 'JavaScript',
         src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433',
         alt: 'JavaScript logo'
       },
       {
+        title: 'React',
         src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207',
         alt: 'React logo'
       }, 
       {
+        title: 'HTML5',
         src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png',
-        alt: 'HTML logo'
+        alt: 'HTML5 logo'
       }, 
       {
+        title: 'CSS3',
         src: 'https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg',
         alt: 'CSS logo'
       },
       {
-        src: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+        title: 'Python',
+        src: 'https://storage.needpix.com/rsynced_images/language-2024210_1280.png',
         alt: 'Python logo'
       }
     ]
@@ -30,10 +35,12 @@ export const skillArray = [
     text: 'Re-orienting or designing from scratch from the ground up for all sizes, testing iterating blah blah...',
     tools: [
       {
+        title: 'Figma',
         src: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg',
         alt: 'Figma logo'
       },
       {
+        title: 'Sketch',
         src: 'https://upload.wikimedia.org/wikipedia/commons/5/59/Sketch_Logo.svg',
         alt: 'Sketch logo'
       }
@@ -44,18 +51,22 @@ export const skillArray = [
     text: 'Task scoping, prioritization, roadmapping etc...',
     tools: [
       {
+        title: 'JIRA',
         src: 'https://cdn.icon-icons.com/icons2/2699/PNG/512/atlassian_jira_logo_icon_170511.png',
         alt: 'JIRA logo'
       }, 
       {
+        title: 'Confluence',
         src: 'https://seeklogo.com/images/C/confluence-logo-D9B07137C2-seeklogo.com.png',
         alt: 'Confluence logo'
       },
       {
+        title: 'Asana',
         src: 'https://cdn.worldvectorlogo.com/logos/asana-logo.svg',
         alt: 'Asana logo'
       },
       {
+        title: 'Zenhub',
         src: 'https://seeklogo.com/images/Z/zenhub-logo-5302123E6B-seeklogo.com.png',
         alt: 'Zenhub logo'
       }
